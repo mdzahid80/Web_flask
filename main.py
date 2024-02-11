2d25744c25f389eb8e69cc5c3d6a9c40b1c91508
zahidgzp80 <zahidgzp80@gmail.com>
2024-02-11 12:18:02 +0530

    Publishing

--- main.py
+++ main.py
@@ -30,11 +30,10 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
-print(SECRET_KEY)
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -61,7 +60,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLite_URI")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
